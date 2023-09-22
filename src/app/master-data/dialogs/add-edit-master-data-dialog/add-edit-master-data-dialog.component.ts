7bfaa125673d163379d996ab4ae1e43f6831bba9
Jing Xuan Goh <gohjx8808@gmail.com>
2023-09-22 21:41:57 +0800

    input in add edit master data dialog

--- src/app/master-data/dialogs/add-edit-master-data-dialog/add-edit-master-data-dialog.component.ts
+++ src/app/master-data/dialogs/add-edit-master-data-dialog/add-edit-master-data-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -7,5 +8,37 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./add-edit-master-data-dialog.component.scss'],
 })
 export class AddEditMasterDataDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: masterData.addEditData) {}
+  title = '';
+  masterDataForm: FormGroup;
+  submitLoading = false;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: masterData.addEditData,
+    formBuilder: NonNullableFormBuilder
+  ) {
+    if (data.isAdd) {
+      this.title = 'Add ';
+    } else {
+      this.title = 'Edit ';
+    }
+
+    if (data.isYarnCategory) {
+      this.title += 'Yarn Category';
+    } else {
+      this.title += 'Yarn Color Category';
+    }
+
+    this.masterDataForm = formBuilder.group({
+      name: [data.data.name, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (!this.masterDataForm.valid) {
+      this.masterDataForm.markAllAsTouched();
+    } else {
+      this.submitLoading = true;
+      console.log(this.masterDataForm.value);
+    }
+  }
 }
