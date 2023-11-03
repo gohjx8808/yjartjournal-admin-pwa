9276e2d6a02dae2c4d4761722254f7c400e2b22d
Jing Xuan Goh <gohjx8808@gmail.com>
2023-11-03 21:42:05 +0800

    added auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,22 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { YarnStockComponent } from './yarn-stock/yarn-stock.component';
 import { MasterDataComponent } from './master-data/master-data.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'yarn-stock', component: YarnStockComponent },
-  { path: 'yarn-category', component: MasterDataComponent },
-  { path: 'yarn-color-category', component: MasterDataComponent },
+  { path: 'dashboard', component: DashboardComponent, canMatch: [authGuard] },
+  { path: 'yarn-stock', component: YarnStockComponent, canMatch: [authGuard] },
+  {
+    path: 'yarn-category',
+    component: MasterDataComponent,
+    canMatch: [authGuard],
+  },
+  {
+    path: 'yarn-color-category',
+    component: MasterDataComponent,
+    canMatch: [authGuard],
+  },
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,11 @@
+import { CanMatchFn, Router } from '@angular/router';
+
+export const authGuard: CanMatchFn = (route, segments) => {
+  const router = new Router();
+
+  if (localStorage.getItem('TOKEN')) {
+    return true;
+  }
+  router.navigate(['']);
+  return false;
+};
